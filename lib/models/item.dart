aed3ae5e9cd790eb63bc4c261dd629833deff4b8
Victor Emanuel <victorers2@gmail.com>
2020-03-14 16:32:56 -0300

    feat(main.dart): creates a simple todo list
    I erased that demo code and built a TODO list using ListView and Dismissable widgets (to slide a
    list item). New TODOs is inserted at appbar and added by pressing a FAB button. While coding this, I
    learned how do alignment and padding in a widget and how to use Row and Icon widgets. I had an idea
    to each swipe direction execute a different action but Dismissable can only dismiss. Next step is to
    use that public package called 'flutter_slidable'.
    
    BREAKING CHANGE: That demo app just turned into a basic todo list. Cause I'm not afraid of radical
    changes.

---
+++ lib/models/item.dart
@@ -0,0 +1,15 @@
+class Item {
+  String title;
+  bool done;
+
+  Item({this.title, this.done});
+
+  Item.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        done = json['done'];
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'done': done,
+      };
+}
